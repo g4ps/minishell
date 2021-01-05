f95859b4f17986a726f6e964125dcad00d2c1b8f
teevee <teevee@students.21-school.ru>
2021-01-05 21:08:56 +0300

    update strdup

--- libft/ft_strdup.c
+++ libft/ft_strdup.c
@@ -3,31 +3,28 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strdup.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: fthemis <fthemis@student.21-school>        +#+  +:+       +#+        */
+/*   By: teevee <teevee@student.21-school.ru>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/11/04 12:54:28 by fthemis           #+#    #+#             */
-/*   Updated: 2020/11/04 12:54:30 by fthemis          ###   ########.fr       */
+/*   Created: 2020/05/27 16:16:18 by teevee            #+#    #+#             */
+/*   Updated: 2020/05/29 22:22:41 by teevee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdlib.h>
 #include "libft.h"
 
-char	*ft_strdup(const char *s1)
+char	*ft_strdup(const char *src)
 {
-	unsigned long	i;
-	char			*ret;
+	char		*copy;
+	size_t		i;
 
+	if (!(copy = (char *)malloc(sizeof(char) * (ft_strlen(src) + 1))))
+		return (NULL);
 	i = 0;
-	while (s1[i] != '\0')
+	while (src[i] != '\0')
+	{
+		copy[i] = src[i];
 		i++;
-	i++;
-	ret = ft_calloc(i, sizeof(char));
-	if (ret == NULL)
-		return (ret);
-	i = 0;
-	while ((ret[i] = s1[i]) != '\0')
-		i++;
-	ret[i] = '\0';
-	return (ret);
+	}
+	copy[i] = '\0';
+	return (copy);
 }
